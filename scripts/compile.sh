3f85500d46a56a6c70f345077a9ea1e05afb70f8
Mészáros Lajos <m_lajos@hotmail.com>
2021-12-13 23:33:59 +0100

    compile script now takes an outputdir argument

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-OUTPUTDIR=$(pwd)/dist
+if [ -z "$OUTPUTDIR" ]; then
+  OUTPUTDIR=$(pwd)/dist
+fi
 
 LEVEL=1
 
-cd $OUTPUTDIR/game/graph/levels/level$LEVEL
+cd "$OUTPUTDIR/game/graph/levels/level$LEVEL"
 
 echo "FTS"
 
 cat fast.fts.json | from-json --ext=fts | implode -b -l --offset=280 --debug --output=fast.fts
 
-cd $OUTPUTDIR/graph/levels/level$LEVEL
+cd "$OUTPUTDIR/graph/levels/level$LEVEL"
 
 echo "LLF"
 
