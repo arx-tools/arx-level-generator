3dfc95f18d2a0647e70f20f3eab19c388603b366
Mészáros Lajos <m_lajos@hotmail.com>
2021-12-13 20:46:18 +0100

    extracted level index into a variable in the compile script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-cd ./dist/game/graph/levels/level1
+LEVEL=1
+
+cd ./dist/game/graph/levels/level$LEVEL
 
 echo "FTS"
 
 cat fast.fts.json | from-json --ext=fts | implode -b -l --offset=280 --debug --output=fast.fts
 
-cd ../../../../graph/levels/level1
+cd ../../../../graph/levels/level$LEVEL
 
 echo "LLF"
 
-cat level1.llf.json | from-json --ext=dlf | implode -b -l --offset=0 --debug --output=level1.llf
+cat level$LEVEL.llf.json | from-json --ext=dlf | implode -b -l --offset=0 --debug --output=level$LEVEL.llf
 
 echo "DLF"
 
-cat level1.dlf.json | from-json --ext=dlf | implode -b -l --offset=8520 --debug --output=level1.dlf
+cat level$LEVEL.dlf.json | from-json --ext=dlf | implode -b -l --offset=8520 --debug --output=level$LEVEL.dlf
 
 echo "done"
 
