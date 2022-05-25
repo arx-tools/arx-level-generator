18c8b405bd363e791de3dc2bcbba33737dd83356
Lajos Meszaros <m_lajos@hotmail.com>
2022-05-25 09:48:28 +0200

    feat bundle code into a single file and utilize sourcemaps

--- new/src/helpers/functions.ts
+++ new/src/helpers/functions.ts
@@ -1,9 +1,10 @@
 export const add = (a: number, b: number) => {
-  return a + b;
-};
+  return a + b
+}
 
-const identity = (x: any) => x;
+const identity = (x: any) => x
 
 export const tmp = () => {
-  return [[1], [2], [3]].flatMap(identity);
-};
+  throw new Error('sajt')
+  return [[1], [2], [3]].flatMap(identity)
+}
