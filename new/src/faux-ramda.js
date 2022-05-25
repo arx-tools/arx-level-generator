4b5108574d4ade03ad9d63cab0d282795427e05e
Lajos Meszaros <m_lajos@hotmail.com>
2022-05-25 15:31:53 +0200

    feat: implement a few functions from faux ramda

--- new/src/faux-ramda.js
+++ new/src/faux-ramda.js
@@ -1,12 +1,9 @@
 export const countBy = () => {}
 export const partition = () => {}
-export const repeat = () => {}
 export const clone = () => {}
 export const flatten = () => {}
-export const uniq = () => {}
 export const isEmpty = () => {}
 export const reduce = () => {}
-export const times = () => {}
 export const identity = () => {}
 export const addIndex = () => {}
 export const without = () => {}
@@ -14,3 +11,18 @@ export const clamp = () => {}
 export const pluck = () => {}
 export const props = () => {}
 export const any = () => {}
+
+// https://stackoverflow.com/a/14438954/1806628
+export const uniq = (values) => {
+  return values.filter((value, index, self) => {
+    return self.indexOf(value) === index
+  })
+}
+
+export const times = (fn, repetitions) => {
+  return [...Array(repetitions)].map(() => fn())
+}
+
+export const repeat = (value, repetitions) => {
+  return Array(repetitions).fill(value)
+}
