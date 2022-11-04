f0bc79dbb7531a65163ccf00fc2f55416996339c
Lajos Meszaros <m_lajos@hotmail.com>
2022-11-04 22:32:33 +0100

    even more scaffolding for the counter-strike map

--- src/projects/counter-strike/maps/cs_italy.ts
+++ src/projects/counter-strike/maps/cs_italy.ts
@@ -6,13 +6,28 @@ import {
   renderPolygonData,
   rotatePolygonData,
 } from '../../../assets/models'
+import { textures } from '../../../assets/textures'
 import { POLY_GLOW, POLY_NO_SHADOW, POLY_STONE, POLY_TRANS } from '../../../constants'
 import { clone, uniq } from '../../../faux-ramda'
 import { MapData, setColor, setTexture } from '../../../helpers'
 import { PosVertex3, RelativeCoords } from '../../../types'
 
-const isFlatPolygon = (polygon: PosVertex3[]) => {
-  return uniq(polygon.map(({ posY }) => posY)).length === 1
+const isTooLargePolygon = (isQuad: boolean, polygon: PosVertex3[]) => {
+  // TODO: this ignores the Y coordinate altogether resulting in a projection rather than the true dimensions of a polygon
+
+  const v = polygon.slice(0, isQuad ? 4 : 3)
+  const xs = v.map(({ posX }) => posX)
+  const zs = v.map(({ posZ }) => posZ)
+
+  const minX = Math.min(...xs)
+  const maxX = Math.max(...xs)
+  const minZ = Math.min(...zs)
+  const maxZ = Math.max(...zs)
+
+  const sizeX = Math.abs(maxX - minX)
+  const sizeZ = Math.abs(maxZ - minZ)
+
+  return sizeX > 100 || sizeZ > 100
 }
 
 // source: https://free3d.com/3d-model/cs-italy-64059.html
@@ -32,8 +47,10 @@ export const createCsItaly = async (pos: RelativeCoords, scale: number, mapData:
   polygons = polygons.slice(10350, 10400)
 
   polygons = polygons.flatMap(({ texture, polygon }, i) => {
-    if (isFlatPolygon(polygon)) {
-      if (polygon.length === 3) {
+    const isQuad = polygon.length === 4
+
+    if (isTooLargePolygon(isQuad, polygon)) {
+      if (isQuad) {
         // TODO: subdivide if larger than 100x100
       } else {
         // TODO: subdivide if larger than 100x100
@@ -42,13 +59,13 @@ export const createCsItaly = async (pos: RelativeCoords, scale: number, mapData:
 
     return [
       { texture, polygon },
-      {
-        texture,
-        polygon: clone(polygon).map((vertex) => {
-          vertex.posY -= 100
-          return vertex
-        }),
-      },
+      // {
+      //   texture,
+      //   polygon: clone(polygon).map((vertex) => {
+      //     vertex.posY -= 100
+      //     return vertex
+      //   }),
+      // },
     ]
   })
 
@@ -56,8 +73,16 @@ export const createCsItaly = async (pos: RelativeCoords, scale: number, mapData:
 
   willThePolygonDataFit('cs_italy.obj', polygons, pos, scale, mapData)
 
-  renderPolygonData(polygons, pos, scale, ({ texture }) => {
+  setTexture(textures.wood.aliciaRoomMur02, mapData)
+
+  renderPolygonData(polygons, pos, scale, ({ polygon, texture, isQuad }) => {
     const textureIdx = parseInt(texture.split('_')[1])
+
+    if (isTooLargePolygon(isQuad, polygon)) {
+      setColor('red', mapData)
+    } else {
+      setColor('black', mapData)
+    }
 
     let flags = POLY_NO_SHADOW
 
@@ -69,14 +94,14 @@ export const createCsItaly = async (pos: RelativeCoords, scale: number, mapData:
       flags |= POLY_GLOW
     }
 
-    setTexture(
-      {
-        path: 'projects/counter-strike/models/cs_italy/textures',
-        src: `cs_italy_texture_${textureIdx}.jpg`,
-        native: false,
-        flags,
-      },
-      mapData,
-    )
+    // setTexture(
+    //   {
+    //     path: 'projects/counter-strike/models/cs_italy/textures',
+    //     src: `cs_italy_texture_${textureIdx}.jpg`,
+    //     native: false,
+    //     flags,
+    //   },
+    //   mapData,
+    // )
   })(mapData)
 }
