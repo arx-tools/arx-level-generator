06437c73caaf2727fa879a129f45361aa9e04a65
Lajos Meszaros <m_lajos@hotmail.com>
2022-12-13 18:14:58 +0100

    remove manipulation of the map for now

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
 import path from 'node:path'
 import { ArxMap } from './ArxMap'
-import { Color } from './Color'
-import { Vector3 } from './Vector3'
 
 // ....
 ;(async () => {
   const { OUTPUTDIR = path.resolve('./dist'), LEVEL = '1' } = process.env
 
   const level1 = await ArxMap.loadLevel(1)
-
-  // -------------------
-
-  // slicing and glitching level1
-  level1.polygons = level1.polygons.slice(30000, 50000).map((polygon) => {
-    polygon.vertices[0].add(new Vector3(0, 10, 0))
-    polygon.vertices[0].color = Color.fromCSS('white')
-    return polygon
-  })
 
   // -------------------
 
