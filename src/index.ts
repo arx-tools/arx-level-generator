37c8a4154d2a0c3347777c0e1a1c5f3d909a5b92
Lajos Meszaros <m_lajos@hotmail.com>
2023-01-02 14:39:43 +0100

    trying to calculate area of a polygon

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,67 @@ import { Vertex } from './Vertex'
 ;(async () => {
   const { OUTPUTDIR = path.resolve(__dirname, './dist'), LEVEL = '1' } = process.env
 
+  const map = await ArxMap.loadLevel(8)
+
+  const getArea = (i: number, j: number, k: number, isQuadPart: boolean, polygon: Polygon) => {
+    const a = polygon.vertices[i].clone().add(polygon.vertices[j]).divideScalar(2).distanceTo(polygon.vertices[k])
+    const b = polygon.vertices[isQuadPart ? i : k].distanceTo(polygon.vertices[j])
+    return (a * b) / 2
+  }
+
+  const triangle = map.polygons.find((polygon) => {
+    return !polygon.isQuad()
+  }) as Polygon
+
+  console.log(triangle.vertices[3])
+  console.log(triangle.area)
+  console.log('-------------------')
+
+  const variants: [number, number, number][] = [
+    [0, 1, 2],
+    [0, 2, 1],
+    [1, 0, 2],
+    [1, 2, 0],
+    [2, 0, 1],
+    [2, 1, 0],
+  ]
+
+  variants.forEach(([a, b, c]) => {
+    console.log(getArea(a, b, c, false, triangle))
+  })
+
+  // const polygon = map.polygons[0]
+
+  // const a = getArea(0, 1, 2, false, polygon)
+  // const b = getArea(1, 2, 0, false, polygon)
+  // const c = getArea(2, 0, 1, false, polygon)
+
+  // const A = getArea(1, 2, 3, true, polygon)
+  // const B = getArea(2, 3, 1, true, polygon)
+  // const C = getArea(3, 1, 2, true, polygon)
+
+  // console.log(polygon.area)
+  // console.log(a + A, a + B, a + C)
+  // console.log(b + A, b + B, b + C)
+  // console.log(c + A, c + B, c + C)
+
+  /*
+  const area = getArea(0, 1, 2, false, polygon)
+
+  let area2 = 0
+  let area3 = 0
+  let area4 = 0
+  if (polygon.isQuad()) {
+    area2 = getArea(1, 2, 3, true, polygon)
+    area3 = getArea(2, 3, 1, true, polygon)
+    area4 = getArea(3, 1, 2, true, polygon)
+  }
+
+  console.log(polygon.area, area + area2, area + area3, area + area4)
+  */
+
+  // --------------
+
   // const map = await ArxMap.loadLevel(2)
 
   // const map2 = await ArxMap.loadLevel(15)
@@ -27,6 +88,7 @@ import { Vertex } from './Vertex'
   //   portcullis.position.add(new Vector3(80, 0, 0))
   // }
 
+  /*
   const map = new ArxMap()
 
   const polygon = new Polygon({
@@ -58,4 +120,5 @@ import { Vertex } from './Vertex'
   map.finalize()
 
   map.saveToDisk(OUTPUTDIR, parseInt(LEVEL))
+  */
 })()
