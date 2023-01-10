b2125891c798cd43aa60a84a9a5bb19a6fb44483
Lajos Meszaros <m_lajos@hotmail.com>
2023-01-10 14:59:39 +0100

    switch to the joined goblin levels project to see if it still works

--- src/index.ts
+++ src/index.ts
@@ -27,71 +27,70 @@ import { Light } from './Light'
 
   // --------------
 
-  // const map = await ArxMap.fromOriginalLevel(0)
+  const map = await ArxMap.fromOriginalLevel(2)
 
-  // const map2 = await ArxMap.fromOriginalLevel(15)
-  // map2.alignPolygonsTo(map)
-  // map.add(map2)
+  const map2 = await ArxMap.fromOriginalLevel(15)
+  map.add(map2, true)
 
-  // map.zones = []
+  map.zones = []
 
-  // // porticullis_0085.move 80 0 0
-  // const portcullis = map.entities.find((entity) => {
-  //   return entity.name.endsWith('porticullis') && entity.id === 85
-  // })
-  // if (typeof portcullis !== 'undefined') {
-  //   portcullis.position.add(new Vector3(80, 0, 0))
-  // }
+  // porticullis_0085.move 80 0 0
+  const portcullis = map.entities.find((entity) => {
+    return entity.name.endsWith('porticullis') && entity.id === 85
+  })
+  if (typeof portcullis !== 'undefined') {
+    portcullis.position.add(new Vector3(80, 0, 0))
+  }
 
-  // map.removePortals()
+  map.removePortals()
 
   // --------------
 
-  const map = new ArxMap()
+  // const map = new ArxMap()
 
-  map.config.offset = new Vector3(1000, 0, 1000)
-  map.player.position.adjustToPlayerHeight()
-  map.hideMinimap()
+  // map.config.offset = new Vector3(1000, 0, 1000)
+  // map.player.position.adjustToPlayerHeight()
+  // map.hideMinimap()
 
-  const backroomsCarpet = await Texture.fromCustomFile({
-    sourcePath: 'projects/the-backrooms/textures/',
-    filename: 'backrooms-[fabric]-carpet-dirty.jpg',
-    isNative: false,
-  })
+  // const backroomsCarpet = await Texture.fromCustomFile({
+  //   sourcePath: 'projects/the-backrooms/textures/',
+  //   filename: 'backrooms-[fabric]-carpet-dirty.jpg',
+  //   isNative: false,
+  // })
 
-  const floorMesh = createFloorMesh(2000, 2000, Color.white.darken(50), backroomsCarpet)
-  makeBumpy(20, 10, floorMesh)
-  const floor = ArxMap.fromThreeJsMesh(floorMesh)
-  floor.config.offset.y += 15
-  floor.polygons.forEach((polygon) => {
-    polygon.flags |= ArxPolygonFlags.Tiled
-  })
-  map.add(floor, true)
+  // const floorMesh = createFloorMesh(2000, 2000, Color.white.darken(50), backroomsCarpet)
+  // makeBumpy(20, 10, floorMesh)
+  // const floor = ArxMap.fromThreeJsMesh(floorMesh)
+  // floor.config.offset.y += 15
+  // floor.polygons.forEach((polygon) => {
+  //   polygon.flags |= ArxPolygonFlags.Tiled
+  // })
+  // map.add(floor, true)
 
-  const water = ArxMap.fromThreeJsMesh(createFloorMesh(2000, 2000, Color.white, Texture.water))
-  water.polygons.forEach((polygon) => {
-    polygon.flags |= ArxPolygonFlags.Tiled | ArxPolygonFlags.Water
-    polygon.setOpacity(10)
-  })
-  map.add(water, true)
+  // const water = ArxMap.fromThreeJsMesh(createFloorMesh(2000, 2000, Color.white, Texture.water))
+  // water.polygons.forEach((polygon) => {
+  //   polygon.flags |= ArxPolygonFlags.Tiled | ArxPolygonFlags.Water
+  //   polygon.setOpacity(10)
+  // })
+  // map.add(water, true)
 
-  const light = new Light({
-    color: Color.yellow.lighten(50),
-    position: new Vector3(0, -800, 0),
-    fallStart: 100,
-    fallEnd: 1000,
-    intensity: 2,
-    lightData: {
-      exFlicker: Color.transparent,
-      exRadius: 0,
-      exFrequency: 0,
-      exSize: 0,
-      exSpeed: 0,
-      exFlareSize: 0,
-    },
-  })
+  // const light = new Light({
+  //   color: Color.yellow.lighten(50),
+  //   position: new Vector3(0, -800, 0),
+  //   fallStart: 100,
+  //   fallEnd: 1000,
+  //   intensity: 2,
+  //   lightData: {
+  //     exFlicker: Color.transparent,
+  //     exRadius: 0,
+  //     exFrequency: 0,
+  //     exSize: 0,
+  //     exSpeed: 0,
+  //     exFlareSize: 0,
+  //   },
+  // })
 
-  map.lights.push(light)
+  // map.lights.push(light)
 
   map.finalize()
 
