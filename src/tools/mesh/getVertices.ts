f914537a90959d5ace5a828cac4685783b667c43
Lajos Meszaros <m_lajos@hotmail.com>
2023-01-24 23:01:26 +0100

    split getVertices implementation into 2 variations

--- src/tools/mesh/getVertices.ts
+++ src/tools/mesh/getVertices.ts
@@ -1,14 +1,38 @@
-import { Vector3 } from '@src/Vector3'
 import { BufferGeometry } from 'three'
+import { Vector3 } from '@src/Vector3'
 
+/**
+ * Gets the vertices of a geometry.
+ * Should be used for stuff, like terrain manipulation as indexed geometries will not be unpacked
+ * into individual vertices.
+ */
 export const getVertices = (geometry: BufferGeometry) => {
+  const vertices: { vector: Vector3; idx: number }[] = []
+
+  const coords = geometry.getAttribute('position')
+
+  for (let idx = 0; idx < coords.count; idx++) {
+    vertices.push({
+      idx,
+      vector: new Vector3(coords.getX(idx), coords.getY(idx), coords.getZ(idx)),
+    })
+  }
+
+  return vertices
+}
+
+/**
+ * Gets the non-indexed version of vertices of a geometry.
+ * Should be used when converting it to Arx polygon data as Arx uses non-indexed geometry.
+ */
+export const getNonIndexedVertices = (geometry: BufferGeometry) => {
+  const vertices: { vector: Vector3; idx: number }[] = []
+
   const index = geometry.getIndex()
   const coords = geometry.getAttribute('position')
 
-  const vertices: { vector: Vector3; idx: number }[] = []
-
   if (index === null) {
-    // non-indexed, all vertices are unique
+    // non-indexed geometry, all vertices are unique
     for (let idx = 0; idx < coords.count; idx++) {
       vertices.push({
         idx,
@@ -16,7 +40,7 @@ export const getVertices = (geometry: BufferGeometry) => {
       })
     }
   } else {
-    // indexed, has shared vertices
+    // indexed geometry, has shared vertices
     for (let i = 0; i < index.count; i++) {
       const idx = index.getX(i)
       vertices.push({
