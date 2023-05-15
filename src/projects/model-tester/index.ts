6b51e159ea0ad7cb598d7f34e5bc4d2de81d744b
Lajos Meszaros <m_lajos@hotmail.com>
2023-05-15 23:31:42 +0200

    add Lory's model from area 51

--- src/projects/model-tester/index.ts
+++ src/projects/model-tester/index.ts
@@ -7,11 +7,13 @@ import { DONT_QUADIFY, SHADING_SMOOTH } from '@src/Polygons.js'
 import { Rotation } from '@src/Rotation.js'
 import { Texture } from '@src/Texture.js'
 import { Vector3 } from '@src/Vector3.js'
+import { Zone } from '@src/Zone.js'
+import { applyTransformations } from '@src/helpers.js'
 import { loadOBJ } from '@tools/mesh/loadOBJ.js'
 import { ArxPolygonFlags } from 'arx-convert/types'
 import path from 'node:path'
 import seedrandom from 'seedrandom'
-import { MathUtils, Vector2 } from 'three'
+import { EdgesGeometry, MathUtils, Shape, ShapeGeometry, Vector2 } from 'three'
 
 const createFloor = async (width: number, height: number) => {
   const mesh = await createPlaneMesh(
@@ -23,6 +25,24 @@ const createFloor = async (width: number, height: number) => {
     }),
   )
   return ArxMap.fromThreeJsMesh(mesh, { tryToQuadify: DONT_QUADIFY })
+}
+
+const createSpawnZone = () => {
+  const shape = new Shape()
+  shape.lineTo(100, 0)
+  shape.lineTo(100, 100)
+  shape.lineTo(0, 100)
+
+  const geometry = new ShapeGeometry(shape)
+  const edge = new EdgesGeometry(geometry)
+  edge.rotateX(MathUtils.degToRad(90))
+
+  const zone = Zone.fromThreejsGeometry(edge, {
+    name: 'spawn',
+    backgroundColor: Color.fromCSS('skyblue'),
+  })
+
+  return zone
 }
 
 export default async () => {
@@ -38,69 +58,84 @@ export default async () => {
   const map = new ArxMap()
   map.meta.mapName = 'Model Tester'
   map.meta.seed = SEED
-  map.config.offset = new Vector3(2000, 0, 2000)
+  map.config.offset = new Vector3(6000, 0, 6000)
   map.player.position.adjustToPlayerHeight()
+  // map.player.orientation.y = MathUtils.degToRad(180)
   map.player.withScript()
   map.hud.hide('all')
 
   map.add(await createFloor(1000, 2000), true)
 
   const ceilingLamp = await loadOBJ('projects/the-backrooms/models/ceiling-lamp/ceiling-lamp', {
-    position: new Vector3(2000, 300, 2500),
+    position: new Vector3(0, 300, 500),
     scale: 50,
     scaleUV: 5,
     materialFlags: ArxPolygonFlags.Glow,
   })
 
   const fountain = await loadOBJ('projects/forest/models/fountain/fountain', {
-    position: new Vector3(2000, 3, 2500),
+    position: new Vector3(0, 3, 500),
     scale: 2,
     scaleUV: new Vector2(1, -1),
   })
 
   const tree = await loadOBJ('models/tree/tree', {
-    position: new Vector3(2300, 0, 2800),
+    position: new Vector3(300, 0, 800),
     scale: 30,
     fallbackTexture: Texture.l2TrollWoodPillar08,
   })
 
   const ladder = await loadOBJ('projects/forest/models/ladder/ladder', {
-    position: new Vector3(2300, 100, 2740),
+    position: new Vector3(300, 100, 740),
     scale: 10,
     rotation: new Rotation(MathUtils.degToRad(-70), 0, 0),
     scaleUV: new Vector2(1, -1),
   })
 
   const cableDrum = await loadOBJ('models/cable-drum/cable-drum', {
-    position: new Vector3(1800, 15, 2600),
+    position: new Vector3(-200, 15, 600),
     scale: 10,
   })
 
   const teddy = await loadOBJ('models/teddy-bear/teddy-bear', {
-    position: new Vector3(1825, 150, 2600),
+    position: new Vector3(-175, 150, 600),
     scale: 10,
     rotation: new Rotation(0, 0, MathUtils.degToRad(30)),
   })
 
   const megaphone = await loadOBJ('models/megaphone/megaphone', {
-    position: new Vector3(2000, 100, 2700),
+    position: new Vector3(0, 100, 700),
     scale: 20,
   })
 
   const speaker = await loadOBJ('models/speaker/speaker', {
-    position: new Vector3(1690, 50, 2850),
+    position: new Vector3(-310, 50, 850),
     scale: 10,
   })
 
   const pole = await loadOBJ('models/pole/pole', {
-    position: new Vector3(1800, 50, 2850),
+    position: new Vector3(-200, 50, 850),
     scale: 10,
     materialFlags: ArxPolygonFlags.None,
   })
 
-  const importedModels = [teddy, tree, cableDrum, ceilingLamp, fountain, ladder, megaphone, speaker, pole].flat()
+  const area51Shaft = await loadOBJ('models/area51-shaft/area51-shaft', {
+    position: new Vector3(-1230, -150, -1100),
+    scale: 40,
+    rotation: new Rotation(0, MathUtils.degToRad(180), 0),
+    fallbackTexture: await Texture.fromCustomFile({
+      filename: '[stone]-concrete.jpg',
+      sourcePath: 'textures',
+    }),
+  })
 
-  importedModels.forEach((mesh) => {
+  const importedModels = [teddy, tree, cableDrum, ceilingLamp, fountain, ladder, megaphone, speaker, pole, area51Shaft]
+
+  importedModels.flat().forEach((mesh) => {
+    mesh.translateX(map.config.offset.x)
+    mesh.translateY(map.config.offset.y)
+    mesh.translateZ(map.config.offset.z)
+    applyTransformations(mesh)
     map.polygons.addThreeJsMesh(mesh, { tryToQuadify: DONT_QUADIFY, shading: SHADING_SMOOTH })
   })
 
@@ -112,6 +147,8 @@ export default async () => {
   map.lights.push(createLight(new Vector3(1000, -200, -1000), 1300))
   map.lights.push(createLight(new Vector3(-1000, -200, 1000), 1300))
   map.lights.push(createLight(new Vector3(-1000, -200, -1000), 1300))
+
+  map.zones.push(createSpawnZone())
 
   map.finalize()
 
