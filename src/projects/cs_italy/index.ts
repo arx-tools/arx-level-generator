e344ce83105fa708f95d69213e01e19b138c35aa
Lajos Meszaros <m_lajos@hotmail.com>
2023-05-27 23:27:55 +0200

    feat(cs_italy): set texture flags and set sky color with spawn zone

--- src/projects/cs_italy/index.ts
+++ src/projects/cs_italy/index.ts
@@ -1,13 +1,14 @@
 import { ArxMap } from '@src/ArxMap.js'
-import { Material } from '@src/Material.js'
-import { DONT_QUADIFY, SHADING_FLAT, SHADING_SMOOTH } from '@src/Polygons.js'
-import { Texture } from '@src/Texture.js'
+import { Color } from '@src/Color.js'
+import { DONT_QUADIFY, SHADING_FLAT } from '@src/Polygons.js'
 import { Vector3 } from '@src/Vector3.js'
 import { applyTransformations } from '@src/helpers.js'
+import { createZone } from '@tools/createZone.js'
 import { loadOBJ } from '@tools/mesh/loadOBJ.js'
 import { ArxPolygonFlags } from 'arx-convert/types'
 import path from 'node:path'
 import seedrandom from 'seedrandom'
+import { Object3D } from 'three'
 
 export default async () => {
   const {
@@ -22,24 +23,55 @@ export default async () => {
   const map = new ArxMap()
   map.meta.mapName = 'cs_italy'
   map.meta.seed = SEED
-  map.config.offset = new Vector3(1000, 0, 1000)
+  map.config.offset = new Vector3(6000, 0, 6000)
   map.player.withScript()
   map.player.position.adjustToPlayerHeight()
+
   map.hud.hide('all')
+
+  const meshes: (Object3D | Object3D[])[] = []
+
+  // ---------------------------------
+
+  const spawn = createZone({
+    name: 'spawn',
+    backgroundColor: Color.fromCSS('#b0d6f5'),
+    drawDistance: 10000,
+  })
+
+  map.zones.push(spawn)
 
   // ---------------------------------
 
   const cs_italy = await loadOBJ('projects/counter-strike/models/cs_italy/cs_italy', {
-    position: new Vector3(50, 1000, 100),
-    scale: 0.008,
-    materialFlags: ArxPolygonFlags.Tiled,
+    position: new Vector3(700, 3200, -2600),
+    scale: new Vector3(-0.025, 0.025, 0.025),
+    materialFlags: (texture) => {
+      if (!texture.filename.startsWith('cs_italy_texture_')) {
+        return undefined
+      }
+
+      let flags = ArxPolygonFlags.None | ArxPolygonFlags.DoubleSided
+
+      const textureIdx = parseInt(texture.filename.split('_')[3])
+
+      if (textureIdx >= 80) {
+        flags |= ArxPolygonFlags.Transparent
+      }
+
+      if (textureIdx === 32 || textureIdx === 43) {
+        flags |= ArxPolygonFlags.Glow
+      }
+
+      return flags
+    },
   })
 
-  const models = [cs_italy].flat()
+  meshes.push(cs_italy)
 
   // ---------------------------------
 
-  models.forEach((mesh) => {
+  meshes.flat().forEach((mesh) => {
     mesh.translateX(map.config.offset.x)
     mesh.translateY(map.config.offset.y)
     mesh.translateZ(map.config.offset.z)
