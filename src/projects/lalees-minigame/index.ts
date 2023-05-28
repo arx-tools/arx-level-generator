a595aab2196166a539b872e481f783bccc2c195b
Lajos Meszaros <m_lajos@hotmail.com>
2023-05-29 00:39:44 +0200

    feat(lalees-minigame): add box art and dvd case model

--- src/projects/lalees-minigame/index.ts
+++ src/projects/lalees-minigame/index.ts
@@ -1,14 +1,18 @@
 import { createPlaneMesh } from '@prefabs/mesh/plane.js'
 import { ArxMap } from '@src/ArxMap.js'
 import { Color } from '@src/Color.js'
-import { SHADING_SMOOTH } from '@src/Polygons.js'
+import { DONT_QUADIFY, SHADING_SMOOTH } from '@src/Polygons.js'
+import { Rotation } from '@src/Rotation.js'
 import { Texture } from '@src/Texture.js'
 import { Vector3 } from '@src/Vector3.js'
+import { applyTransformations } from '@src/helpers.js'
 import { createZone } from '@tools/createZone.js'
+import { loadOBJ } from '@tools/mesh/loadOBJ.js'
 import { makeBumpy } from '@tools/mesh/makeBumpy.js'
+import { ArxPolygonFlags } from 'arx-convert/types'
 import path from 'node:path'
 import seedrandom from 'seedrandom'
-import { Vector2 } from 'three'
+import { MathUtils, Vector2 } from 'three'
 import { floor } from 'three/examples/jsm/nodes/Nodes.js'
 
 export default async () => {
@@ -40,6 +44,21 @@ export default async () => {
   makeBumpy(10, 30, false, floorMesh.geometry)
   const floor = ArxMap.fromThreeJsMesh(floorMesh, { tryToQuadify: "don't quadify", shading: SHADING_SMOOTH })
   map.add(floor, true)
+
+  const pcGameBox = await loadOBJ('projects/lalees-minigame/evm-box', {
+    position: new Vector3(100, -100, 100),
+    scale: 0.1,
+    rotation: new Rotation(MathUtils.degToRad(-45), MathUtils.degToRad(-45 - 90), 0),
+    materialFlags: ArxPolygonFlags.None,
+  })
+  const models = [pcGameBox].flat()
+  models.forEach((mesh) => {
+    mesh.translateX(map.config.offset.x)
+    mesh.translateY(map.config.offset.y)
+    mesh.translateZ(map.config.offset.z)
+    applyTransformations(mesh)
+    map.polygons.addThreeJsMesh(mesh, { tryToQuadify: DONT_QUADIFY, shading: SHADING_SMOOTH })
+  })
 
   const spawnZone = createZone({
     name: 'spawn',
