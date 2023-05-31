69cb130cf383356597b391d9ed96e7923a3e6acb
Lajos Meszaros <m_lajos@hotmail.com>
2023-05-31 22:54:29 +0200

    refactor(HUD): change element property to elementVisibility

--- src/HUD.ts
+++ src/HUD.ts
@@ -13,7 +13,7 @@ export enum HudElements {
 }
 
 export class HUD {
-  elements: Record<HudElements, boolean> = {
+  elementVisibility: Record<HudElements, boolean> = {
     [HudElements.Minimap]: true,
     [HudElements.Healthbar]: true,
     [HudElements.Manabar]: true,
@@ -27,34 +27,34 @@ export class HUD {
 
   hide(hudElement: HudElements | 'all') {
     if (hudElement === 'all') {
-      for (let key in this.elements) {
-        this.elements[key as HudElements] = false
+      for (let key in this.elementVisibility) {
+        this.elementVisibility[key as HudElements] = false
       }
     } else {
-      this.elements[hudElement] = false
+      this.elementVisibility[hudElement] = false
     }
   }
 
   show(hudElement: HudElements | 'all') {
     if (hudElement === 'all') {
-      for (let key in this.elements) {
-        this.elements[key as HudElements] = true
+      for (let key in this.elementVisibility) {
+        this.elementVisibility[key as HudElements] = true
       }
     } else {
-      this.elements[hudElement] = true
+      this.elementVisibility[hudElement] = true
     }
   }
 
   exportSourcesAndTargets(outputDir: string, levelIdx: number) {
     const files: Record<string, string> = {}
 
-    if (this.elements[HudElements.Minimap] === false) {
+    if (this.elementVisibility[HudElements.Minimap] === false) {
       const source = path.resolve('assets', 'reset/blank-360x532.bmp')
       const target = path.resolve(outputDir, `graph/levels/level${levelIdx}/map.bmp`)
       files[target] = source
     }
 
-    if (this.elements[HudElements.Healthbar] === false) {
+    if (this.elementVisibility[HudElements.Healthbar] === false) {
       const source1 = path.resolve('assets', 'reset/blank-330x800.bmp')
       const target1 = path.resolve(outputDir, 'graph/interface/bars/empty_gauge_red.bmp')
       files[target1] = source1
@@ -64,7 +64,7 @@ export class HUD {
       files[target2] = source2
     }
 
-    if (this.elements[HudElements.Manabar] === false) {
+    if (this.elementVisibility[HudElements.Manabar] === false) {
       const source1 = path.resolve('assets', 'reset/blank-330x800.bmp')
       const target1 = path.resolve(outputDir, 'graph/interface/bars/empty_gauge_blue.bmp')
       files[target1] = source1
@@ -74,37 +74,37 @@ export class HUD {
       files[target2] = source2
     }
 
-    if (this.elements[HudElements.StealthIndicator] === false) {
+    if (this.elementVisibility[HudElements.StealthIndicator] === false) {
       const source = path.resolve('assets', 'reset/blank-32x32.bmp')
       const target = path.resolve(outputDir, 'graph/interface/icons/stealth_gauge.bmp')
       files[target] = source
     }
 
-    if (this.elements[HudElements.StealingIcon] === false) {
+    if (this.elementVisibility[HudElements.StealingIcon] === false) {
       const source = path.resolve('assets', 'reset/blank-128x128.bmp')
       const target = path.resolve(outputDir, 'graph/interface/icons/steal.bmp')
       files[target] = source
     }
 
-    if (this.elements[HudElements.LevelUpIcon] === false) {
+    if (this.elementVisibility[HudElements.LevelUpIcon] === false) {
       const source = path.resolve('assets', 'reset/blank-32x32.bmp')
       const target = path.resolve(outputDir, 'graph/interface/icons/lvl_up.bmp')
       files[target] = source
     }
 
-    if (this.elements[HudElements.BookIcon] === false) {
+    if (this.elementVisibility[HudElements.BookIcon] === false) {
       const source = path.resolve('assets', 'reset/blank-32x32.bmp')
       const target = path.resolve(outputDir, 'graph/interface/icons/book.bmp')
       files[target] = source
     }
 
-    if (this.elements[HudElements.BackpackIcon] === false) {
+    if (this.elementVisibility[HudElements.BackpackIcon] === false) {
       const source = path.resolve('assets', 'reset/blank-32x32.bmp')
       const target = path.resolve(outputDir, 'graph/interface/icons/backpack.bmp')
       files[target] = source
     }
 
-    if (this.elements[HudElements.PurseIcon] === false) {
+    if (this.elementVisibility[HudElements.PurseIcon] === false) {
       const source = path.resolve('assets', 'reset/blank-32x32.bmp')
       const target = path.resolve(outputDir, 'graph/interface/inventory/gold.bmp')
       files[target] = source
