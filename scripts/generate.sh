22ce1e52b9a5e2a7e9924f0be5a03758c11c3405
Lajos Meszaros <m_lajos@hotmail.com>
2023-06-06 22:21:45 +0200

    fix(scripts): fix typo in generate.sh and call the node package files using the .bin folder executable

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,6 +2,6 @@
 
 REPO_ROOT="$(realpath $(dirname "$(realpath "${BASH_SOURCE:-$0}")")/../)/"
 
-tsc
-tsc-alias
-node --require dotenv/config --enable-source-maps "${REPO_ROOT}build/projects/index.js
+${REPO_ROOT}node_modules/.bin/tsc
+${REPO_ROOT}node_modules/.bin/tsc-alias
+node --require dotenv/config --enable-source-maps "${REPO_ROOT}build/projects/index.js"
