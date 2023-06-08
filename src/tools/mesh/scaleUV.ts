2ac30207f0106c62056f4282c7c4a9084d79cea5
Lajos Meszaros <m_lajos@hotmail.com>
2023-06-08 16:27:00 +0200

    docs(tools): add more info on how to use scaleUV

--- src/tools/mesh/scaleUV.ts
+++ src/tools/mesh/scaleUV.ts
@@ -1,10 +1,15 @@
 import { BufferAttribute, BufferGeometry, Vector2 } from 'three'
 
 /**
- * multiplies uv coordinates in a way that allows textures to be tiled
+ * multiplies uv coordinates in a way that allows textures to be tiled or flipped
  *
  * `scaleUV(new Vector2(2, 3), geometry)` will shrink the texture in a way
  * that the texture will fit 2 times horizontally and 3 times vertically
+ *
+ * `scaleUV(new Vector(-1, 1), geometry)` will flip the texture horizontally
+ * `scaleUV(new Vector(1, -1), geometry)` will flip the texture vertically
+ *
+ * only works with tileable textures
  */
 export const scaleUV = (scale: Vector2, geometry: BufferGeometry) => {
   const uv = geometry.getAttribute('uv') as BufferAttribute
@@ -13,5 +18,6 @@ export const scaleUV = (scale: Vector2, geometry: BufferGeometry) => {
   for (let i = 0; i < uv.count; i++) {
     newUV.push(uv.array[i * uv.itemSize] * scale.x, uv.array[i * uv.itemSize + 1] * scale.y)
   }
+
   geometry.setAttribute('uv', new BufferAttribute(Float32Array.from(newUV), uv.itemSize))
 }
