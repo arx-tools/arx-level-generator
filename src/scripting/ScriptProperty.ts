c614e711d61e01745d4eec50e0d7fa3f934d6fb2
Lajos Meszaros <m_lajos@hotmail.com>
2023-06-15 20:36:44 +0200

    feat(scripting): make ScriptProperty extend ScriptCommand

--- src/scripting/ScriptProperty.ts
+++ src/scripting/ScriptProperty.ts
@@ -1,9 +1,10 @@
-export abstract class ScriptProperty<T> {
+import { ScriptCommand } from '@scripting/ScriptCommand.js'
+
+export abstract class ScriptProperty<T> extends ScriptCommand {
   value: T
 
   constructor(value: T) {
+    super()
     this.value = value
   }
-
-  abstract toString(): string | Promise<string>
 }
