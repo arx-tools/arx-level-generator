78e1970cb6ea96f0f8e7f73b123eb7b03bdc7a83
Lajos Meszaros <m_lajos@hotmail.com>
2023-06-15 22:12:53 +0200

    refactor(scripting): simplify ScriptSubroutine's command evaluation using Script.handlerToString

--- src/scripting/ScriptSubroutine.ts
+++ src/scripting/ScriptSubroutine.ts
@@ -1,6 +1,4 @@
-import { ScriptHandler } from '@src/Script.js'
-import { repeat } from '@src/faux-ramda.js'
-import { ScriptCommand } from '@scripting/ScriptCommand.js'
+import { Script, ScriptHandler } from '@src/Script.js'
 
 export class ScriptSubroutine {
   name: string
@@ -12,13 +10,7 @@ export class ScriptSubroutine {
   }
 
   async toString() {
-    const indentation = repeat(' ', 2).join('')
-    const commands = (this.command instanceof ScriptCommand ? await this.command.toString() : await this.command())
-      .trim()
-      .split('\n')
-      .map((line) => indentation + line.trim())
-
-    return [`>>${this.name} {`, ...commands, indentation + 'return', '}'].join('\n')
+    return [`>>${this.name} {`, await Script.handlerToString(this.command), '  return', '}'].join('\n')
   }
 
   invoke() {
