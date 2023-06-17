d29874c5e2961c30ad3c00c7b6ea082e7d48ccb3
Lajos Meszaros <m_lajos@hotmail.com>
2023-06-18 01:41:16 +0200

    revert(tools): reintroduce generic in toArxCoordinateSystem

--- src/tools/mesh/toArxCoordinateSystem.ts
+++ src/tools/mesh/toArxCoordinateSystem.ts
@@ -1,12 +1,15 @@
 import { BufferGeometry, Object3D } from 'three'
 import { applyTransformations } from '@src/helpers.js'
 
-export const toArxCoordinateSystem = (threeJsObj: Object3D | BufferGeometry) => {
+// the generic syntax will make sure that if we get a Group as input, which is a descendant of Object3D
+// then we'll get the same type returned. Overloads or union types will return strictly from the given types
+export const toArxCoordinateSystem = <T extends Object3D | BufferGeometry>(threeJsObj: T) => {
   if (threeJsObj instanceof BufferGeometry) {
     threeJsObj.scale(1, -1, 1)
   } else {
     threeJsObj.scale.y *= -1
     applyTransformations(threeJsObj)
   }
+
   return threeJsObj
 }
