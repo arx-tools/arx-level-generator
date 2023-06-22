e09e1d7c4815979823f9dc03c5856b4914bc3c3e
Lajos Meszaros <m_lajos@hotmail.com>
2023-06-22 23:14:57 +0200

    feat(package): create package exports

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+export { Ambience } from '@src/Ambience.js'
+export { AmbienceTrack } from '@src/AmbienceTrack.js'
+export { ArxMap } from '@src/ArxMap.js'
+export { Audio } from '@src/Audio.js'
+export { Color } from '@src/Color.js'
+export { Entities } from '@src/Entities.js'
+export { Entity } from '@src/Entity.js'
+export { Fog } from '@src/Fog.js'
+export { HUD } from '@src/HUD.js'
+export { LevelLoader } from '@src/LevelLoader.js'
+export { Light } from '@src/Light.js'
+export { Lights } from '@src/Lights.js'
+export { Material } from '@src/Material.js'
+export { MetaData } from '@src/MetaData.js'
+export { Path } from '@src/Path.js'
+export { Player } from '@src/Player.js'
+export { Polygon } from '@src/Polygon.js'
+export { Polygons } from '@src/Polygons.js'
+export { Portal } from '@src/Portal.js'
+export { Rotation } from '@src/Rotation.js'
+export { Script } from '@src/Script.js'
+export { Texture } from '@src/Texture.js'
+export { Translations } from '@src/Translations.js'
+export { UI } from '@src/UI.js'
+export { Vectors } from '@src/Vectors.js'
+export { Vector3 } from '@src/Vector3.js'
+export { Vertex } from '@src/Vertex.js'
+export { Zone } from '@src/Zone.js'
+
+// errors.ts
+// faux-ramda.ts
+// globals.d.ts
+// helpers.ts
+// random.ts
---
+++ src/prefabs/index.ts
@@ -0,0 +1,21 @@
+export { Cube } from '@prefabs/entity/Cube.js'
+export {
+  Door,
+  Portcullis,
+  LightDoor,
+  CatacombHeavyDoor,
+  YlsideDoor,
+  GoblinVeryLightDoor,
+} from '@prefabs/entity/Door.js'
+export { Lever } from '@prefabs/entity/Lever.js'
+export { Marker } from '@prefabs/entity/Marker.js'
+export { Rune } from '@prefabs/entity/Rune.js'
+export { SoundPlayer } from '@prefabs/entity/SoundPlayer.js'
+
+export { createBox } from '@prefabs/mesh/box.js'
+export { createPlaneMesh } from '@prefabs/mesh/plane.js'
+
+export { Cursor } from '@prefabs/rooms/Cursor.js'
+export { Rooms } from '@prefabs/rooms/Rooms.js'
+export { loadRooms } from '@prefabs/rooms/loadRooms.js'
+export { createRoom } from '@prefabs/rooms/room.js'
