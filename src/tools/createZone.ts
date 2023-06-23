2ce3c9272e3932e4e102b51c9e42a5afa71bd193
Lajos Meszaros <m_lajos@hotmail.com>
2023-06-23 23:28:56 +0200

    feat(Zone): offset zone to be in the center of the rectangle set by the size property

--- src/tools/createZone.ts
+++ src/tools/createZone.ts
@@ -6,8 +6,8 @@ import { Zone, ZoneConstructorProps } from '@src/Zone.js'
 export const createZone = (
   props: Expand<Omit<ZoneConstructorProps, 'points' | 'height'> & { position?: Vector3; size?: Vector3 }>,
 ) => {
-  const position = props.position ?? new Vector3(0, 0, 0)
   const size = props.size ?? new Vector3(100, Infinity, 100)
+  const position = props.position ?? new Vector3(0, 0, 0)
 
   const shape = new Shape()
   shape.lineTo(size.x, 0)
@@ -17,7 +17,7 @@ export const createZone = (
   const geometry = new ShapeGeometry(shape)
   const edge = new EdgesGeometry(geometry)
   edge.rotateX(MathUtils.degToRad(90))
-  edge.translate(position.x, position.y, position.z)
+  edge.translate(position.x - size.x / 2, position.y, position.z - size.z / 2)
 
   return Zone.fromThreejsGeometry(edge, {
     height: size.y,
