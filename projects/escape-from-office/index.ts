2dc6f00883acd1cce8497da494236d73c17d15a9
Lajos Meszaros <m_lajos@hotmail.com>
2023-07-06 23:04:49 +0200

    fix(escape-from-office): remove models

--- projects/escape-from-office/index.ts
+++ projects/escape-from-office/index.ts
@@ -1,18 +1,10 @@
 import path from 'node:path'
-import { ArxPolygonFlags } from 'arx-convert/types'
 import seedrandom from 'seedrandom'
-import { MathUtils } from 'three'
 import { ArxMap } from '@src/ArxMap.js'
 import { Color } from '@src/Color.js'
-import { DONT_QUADIFY, SHADING_FLAT, SHADING_SMOOTH } from '@src/Polygons.js'
-import { Rotation } from '@src/Rotation.js'
 import { Vector3 } from '@src/Vector3.js'
-import { applyTransformations } from '@src/helpers.js'
 import { loadRooms } from '@prefabs/rooms/loadRooms.js'
 import { createZone } from '@tools/createZone.js'
-import { loadOBJ } from '@tools/mesh/loadOBJ.js'
-
-// import { createLight } from '@tools/createLight.js'
 
 export default async () => {
   const {
@@ -39,28 +31,6 @@ export default async () => {
     map.add(room, true)
   })
 
-  const wetFloorSign = await loadOBJ('models/wet-floor-sign/wet-floor-sign', {
-    position: new Vector3(-250, -30, 410),
-    rotation: new Rotation(0, MathUtils.degToRad(30), 0),
-    scale: 0.3,
-    materialFlags: ArxPolygonFlags.NoShadow,
-  })
-
-  const ceilingLampOffice1 = await loadOBJ('models/ceiling-lamp/ceiling-lamp', {
-    position: new Vector3(-24, -308, 54),
-    scale: 0.65,
-    materialFlags: ArxPolygonFlags.NoShadow | ArxPolygonFlags.Glow,
-  })
-
-  const ceilingLampOffice2 = await loadOBJ('models/ceiling-lamp/ceiling-lamp', {
-    position: new Vector3(-24, -308, 54 + 840),
-    scale: 0.65,
-    materialFlags: ArxPolygonFlags.NoShadow | ArxPolygonFlags.Glow,
-  })
-
-  const hardModels = [...wetFloorSign]
-  const smoothModels = [...ceilingLampOffice1, ...ceilingLampOffice2]
-
   const spawnZone = createZone({
     position: new Vector3(0, 10, 0),
     name: 'spawn',
@@ -69,27 +39,7 @@ export default async () => {
 
   const zones = [spawnZone]
 
-  // const sun = createLight({ position: new Vector3(0, -200, -1000), radius: 2000 })
-
-  // const lights = [sun]
-
   // --------------
-
-  hardModels.forEach((mesh) => {
-    mesh.translateX(map.config.offset.x)
-    mesh.translateY(map.config.offset.y)
-    mesh.translateZ(map.config.offset.z)
-    applyTransformations(mesh)
-    map.polygons.addThreeJsMesh(mesh, { tryToQuadify: DONT_QUADIFY, shading: SHADING_FLAT })
-  })
-
-  smoothModels.forEach((mesh) => {
-    mesh.translateX(map.config.offset.x)
-    mesh.translateY(map.config.offset.y)
-    mesh.translateZ(map.config.offset.z)
-    applyTransformations(mesh)
-    map.polygons.addThreeJsMesh(mesh, { tryToQuadify: DONT_QUADIFY, shading: SHADING_SMOOTH })
-  })
 
   map.zones.push(...zones)
   // map.lights.push(...lights)
