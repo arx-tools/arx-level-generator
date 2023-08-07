1adc0d42da0e8c441999977d1ed3e335c3ccf10f
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-07 20:53:33 +0200

    feat(compile): prepare a function to be called to trigger compilation

---
+++ src/compile.ts
@@ -0,0 +1,8 @@
+import { Settings } from '@src/Settings.js'
+
+export const compile = async (settings: Settings) => {
+  // compile FTS
+  // compile LLF
+  // compile DLF
+  // calculate lighting
+}
