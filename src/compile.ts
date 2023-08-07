82028866ddfae8d798d0ba245f7743438360f7f3
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-07 23:56:39 +0200

    feat(compile): compile DLF and LLF files

--- src/compile.ts
+++ src/compile.ts
@@ -1,17 +1,15 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import { Readable } from 'node:stream'
-import { FTS } from 'arx-convert'
-import { ArxFTS } from 'arx-convert/types'
+import { DLF, FTS, LLF } from 'arx-convert'
+import { ArxDLF, ArxFTS, ArxLLF } from 'arx-convert/types'
 import { getHeaderSize } from 'arx-header-size'
 import { Compression, DictionarySize, implode, stream } from 'node-pkware'
 import { Settings } from '@src/Settings.js'
 
 const { through, transformSplitBy, splitAt, transformIdentity } = stream
 
-export const compile = async (settings: Settings) => {
-  // compile FTS
-
+const compileFTS = async (settings: Settings) => {
   const ftsPath = path.join(settings.outputDir, `game/graph/levels/level${settings.levelIdx}`)
   const ftsJSONRaw = await fs.promises.readFile(path.join(ftsPath, 'fast.fts.json'), 'utf-8')
   const ftsJSON = JSON.parse(ftsJSONRaw) as ArxFTS
@@ -30,8 +28,52 @@ export const compile = async (settings: Settings) => {
       ),
     )
     .pipe(fs.createWriteStream(path.join(ftsPath, 'fast.fts')))
+}
 
-  // compile LLF
-  // compile DLF
+const compileLLF = async (settings: Settings) => {
+  const llfPath = path.join(settings.outputDir, `graph/levels/level${settings.levelIdx}`)
+  const llfJSONRaw = await fs.promises.readFile(path.join(llfPath, `level${settings.levelIdx}.llf.json`), 'utf-8')
+  const llfJSON = JSON.parse(llfJSONRaw) as ArxLLF
+
+  const repackedLlf = LLF.save(llfJSON)
+  const { total: llfHeaderSize } = getHeaderSize(repackedLlf, 'llf')
+
+  Readable.from(repackedLlf)
+    .pipe(
+      through(
+        transformSplitBy(
+          splitAt(llfHeaderSize),
+          transformIdentity(),
+          implode(Compression.Binary, DictionarySize.Large),
+        ),
+      ),
+    )
+    .pipe(fs.createWriteStream(path.join(llfPath, `level${settings.levelIdx}.llf`)))
+}
+
+const compileDLF = async (settings: Settings) => {
+  const dlfPath = path.join(settings.outputDir, `graph/levels/level${settings.levelIdx}`)
+  const dlfJSONRaw = await fs.promises.readFile(path.join(dlfPath, `level${settings.levelIdx}.dlf.json`), 'utf-8')
+  const dlfJSON = JSON.parse(dlfJSONRaw) as ArxDLF
+
+  const repackedDlf = DLF.save(dlfJSON)
+  const { total: dlfHeaderSize } = getHeaderSize(repackedDlf, 'dlf')
+
+  Readable.from(repackedDlf)
+    .pipe(
+      through(
+        transformSplitBy(
+          splitAt(dlfHeaderSize),
+          transformIdentity(),
+          implode(Compression.Binary, DictionarySize.Large),
+        ),
+      ),
+    )
+    .pipe(fs.createWriteStream(path.join(dlfPath, `level${settings.levelIdx}.dlf`)))
+}
+
+export const compile = async (settings: Settings) => {
+  await Promise.all([compileFTS(settings), compileLLF(settings), compileDLF(settings)])
+
   // calculate lighting
 }
