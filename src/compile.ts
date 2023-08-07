d2a758d48ab5ab05726c4c245caa09ebc9386392
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-07 23:47:03 +0200

    feat(compile): compile FTS files

--- src/compile.ts
+++ src/compile.ts
@@ -1,7 +1,36 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { Readable } from 'node:stream'
+import { FTS } from 'arx-convert'
+import { ArxFTS } from 'arx-convert/types'
+import { getHeaderSize } from 'arx-header-size'
+import { Compression, DictionarySize, implode, stream } from 'node-pkware'
 import { Settings } from '@src/Settings.js'
+
+const { through, transformSplitBy, splitAt, transformIdentity } = stream
 
 export const compile = async (settings: Settings) => {
   // compile FTS
+
+  const ftsPath = path.join(settings.outputDir, `game/graph/levels/level${settings.levelIdx}`)
+  const ftsJSONRaw = await fs.promises.readFile(path.join(ftsPath, 'fast.fts.json'), 'utf-8')
+  const ftsJSON = JSON.parse(ftsJSONRaw) as ArxFTS
+
+  const repackedFts = FTS.save(ftsJSON)
+  const { total: ftsHeaderSize } = getHeaderSize(repackedFts, 'fts')
+
+  Readable.from(repackedFts)
+    .pipe(
+      through(
+        transformSplitBy(
+          splitAt(ftsHeaderSize),
+          transformIdentity(),
+          implode(Compression.Binary, DictionarySize.Large),
+        ),
+      ),
+    )
+    .pipe(fs.createWriteStream(path.join(ftsPath, 'fast.fts')))
+
   // compile LLF
   // compile DLF
   // calculate lighting
