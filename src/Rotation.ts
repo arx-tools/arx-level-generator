6ad0394537124ee98f4431c3b6cdfa8d8dcdd334
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-13 14:03:18 +0200

    feat(Rotation): implement reorder which returns this instead of an Euler instance

--- src/Rotation.ts
+++ src/Rotation.ts
@@ -1,18 +1,28 @@
 import { ArxRotation } from 'arx-convert/types'
-import { Euler, MathUtils } from 'three'
+import { Euler, EulerOrder, MathUtils } from 'three'
 
 export class Rotation extends Euler {
   static fromArxRotation({ a, b, g }: ArxRotation) {
     return new Rotation(MathUtils.degToRad(a), MathUtils.degToRad(b), MathUtils.degToRad(g), 'XYZ')
   }
 
+  reorder(newOrder: EulerOrder) {
+    const { x, y, z } = super.reorder(newOrder)
+
+    this.x = x
+    this.y = y
+    this.z = z
+
+    return this
+  }
+
   toArxRotation(): ArxRotation {
-    const { x, y, z } = this.reorder('XYZ')
+    this.reorder('XYZ')
 
     return {
-      a: MathUtils.radToDeg(x),
-      b: MathUtils.radToDeg(y),
-      g: MathUtils.radToDeg(z),
+      a: MathUtils.radToDeg(this.x),
+      b: MathUtils.radToDeg(this.y),
+      g: MathUtils.radToDeg(this.z),
     }
   }
 }
