cd9ce17de523bb04280d0003aaececd32bba8b84
Lajos Meszaros <m_lajos@hotmail.com>
2023-09-23 15:38:38 +0200

    feat(scripting/hooks/useDelay): add uniqueDelay function

--- src/scripting/hooks/useDelay.ts
+++ src/scripting/hooks/useDelay.ts
@@ -3,9 +3,25 @@ let delayIdx = 0
 export const useDelay = () => {
   let delayOffset = 0
 
-  return (delayInMs: number = 0) => {
+  /**
+   * creates a timer with a unique identifier
+   */
+  const delay = (delayInMs: number = 0) => {
     delayOffset += Math.floor(delayInMs)
 
     return `TIMERdelay${++delayIdx} -m 1 ${delayOffset}`
   }
+
+  /**
+   * creates a timer without any identification
+   * so the delay stays unique at runtime
+   * and consequently being uncancellable with the off parameter
+   */
+  const uniqueDelay = (delayInMs: number = 0) => {
+    delayOffset += Math.floor(delayInMs)
+
+    return `TIMER -m 1 ${delayOffset}`
+  }
+
+  return { delay, uniqueDelay }
 }
