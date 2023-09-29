d95cd564d166ecc150806ac29c7b8b316c738971
Lajos Meszaros <m_lajos@hotmail.com>
2023-09-30 00:43:19 +0200

    feat(prefabs/rooms): add premium only block syntax to rooms files

--- src/prefabs/rooms/loadRooms.ts
+++ src/prefabs/rooms/loadRooms.ts
@@ -27,12 +27,18 @@ export const loadRooms = async (filename: string, settings: Settings) => {
   const rawInput = await fs.promises.readFile(path.resolve(settings.assetsDir, filename), 'utf-8')
   const lines = rawInput.split(/\r?\n/)
 
-  let currentBlock:
+  type CurrentBlock =
     | {
         type: 'define'
         name: string
       }
-    | undefined = undefined
+    | {
+        type: 'premium'
+      }
+
+  let currentBlock: CurrentBlock | undefined = undefined
+
+  let premiumLinesSkipped = 0
 
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i].replace(/#.*$/, '').trim()
@@ -43,138 +49,238 @@ export const loadRooms = async (filename: string, settings: Settings) => {
 
     const tokens = line.split(' ')
 
-    if (currentBlock !== undefined) {
+    const parseCursorKeyword = () => {
+      switch (tokens[1]) {
+        case 'save':
+          // TODO: check if tokens[2] exists
+          cursor.saveAs(tokens[2])
+          break
+        case 'restore':
+          // TODO: check if tokens[2] exists
+          cursor.restore(tokens[2])
+          break
+        default:
+          console.error(`[error] loadRooms: Unknown parameter "${tokens[1]}" after "cursor" at line ${i + 1}`)
+      }
+    }
+
+    const parseWithKeyword = () => {
+      switch (tokens[1]) {
+        case 'light':
+          if (typeof rooms.currentRoom === 'undefined') {
+            // TODO: error: only add light if there's at least 1 room
+          } else {
+            const mainLight = createLight({
+              radius: Math.max(cursor.newSize.x, cursor.newSize.y, cursor.newSize.z),
+              position: new Vector3(cursor.cursor.x, cursor.cursor.y - cursor.newSize.y / 2, cursor.cursor.z),
+            })
+
+            rooms.currentRoom.lights.push(mainLight)
+          }
+          break
+        default:
+          console.error(`[error] loadRooms: Unknown parameter "${tokens[1]}" after "with" at line ${i + 1}`)
+      }
+    }
+
+    const parseRoomKeyword = () => {
+      switch (tokens[1]) {
+        case 'add':
+          // TODO: validate arguments
+          const posX = parseInt(tokens[2].startsWith('$') ? variables[tokens[2]] : tokens[2])
+          const posY = parseInt(tokens[3].startsWith('$') ? variables[tokens[3]] : tokens[3])
+          const posZ = parseInt(tokens[4].startsWith('$') ? variables[tokens[4]] : tokens[4])
+          let definitionName = tokens[5]
+          if (typeof roomDefinitions[definitionName] === 'undefined') {
+            console.error(`[error] loadRooms: Unknown texture definition "${tokens[5]}" at line ${i + 1}`)
+            definitionName = 'default'
+          }
+          const adjustments = tokens.slice(6) as CursorDir[]
+
+          rooms.addRoom(new Vector3(posX, posY, posZ), roomDefinitions[definitionName], ...adjustments)
+          break
+        default:
+          console.error(`[error] loadRooms: Unknown parameter "${tokens[1]}" after "room" at line ${i + 1}`)
+      }
+    }
+
+    const parseVariable = () => {
+      if (tokens[1] === '=') {
+        if (typeof tokens[2] !== 'undefined') {
+          variables[tokens[0]] = tokens[2]
+        } else {
+          console.error(`[error] loadRooms: Missing value for variable at line ${i + 1}`)
+        }
+      } else {
+        console.error(`[error] loadRooms: Unexpected token "${tokens[1]}" after variable at line ${i + 1}`)
+      }
+    }
+
+    if (typeof currentBlock !== 'undefined') {
       switch (currentBlock.type) {
         case 'define':
-          switch (tokens[0]) {
-            case '}':
-              currentBlock = undefined
-              break
-            case 'floor':
-            case 'ceiling':
-              {
-                if (tokens[1] === 'custom') {
-                  roomDefinitions[currentBlock.name].textures[tokens[0]].texture = Texture.fromCustomFile({
-                    filename: tokens[3],
-                    sourcePath: tokens[2],
-                  })
-                } else if (tokens[1] === 'arx') {
-                  // TODO: an arx texture might not always be 128x128
-                  roomDefinitions[currentBlock.name].textures[tokens[0]].texture = new Texture({
-                    filename: tokens[2],
-                    size: 128,
-                  })
-                } else if (tokens[1] === 'removed') {
-                  roomDefinitions[currentBlock.name].textures[tokens[0]].isRemoved = true
-                } else {
-                  console.error(
-                    `[error] loadRooms: Unknown texture type "${tokens[1]}" at line ${
-                      i + 1
-                    }, expected either "custom", "arx" or "removed"`,
-                  )
+          {
+            switch (tokens[0]) {
+              case '}':
+                currentBlock = undefined
+                break
+              case 'floor':
+              case 'ceiling':
+                {
+                  if (tokens[1] === 'custom') {
+                    roomDefinitions[currentBlock.name].textures[tokens[0]].texture = Texture.fromCustomFile({
+                      filename: tokens[3],
+                      sourcePath: tokens[2],
+                    })
+                  } else if (tokens[1] === 'arx') {
+                    // TODO: an arx texture might not always be 128x128
+                    roomDefinitions[currentBlock.name].textures[tokens[0]].texture = new Texture({
+                      filename: tokens[2],
+                      size: 128,
+                    })
+                  } else if (tokens[1] === 'off') {
+                    roomDefinitions[currentBlock.name].textures[tokens[0]].isRemoved = true
+                  } else {
+                    console.error(
+                      `[error] loadRooms: Unknown texture type "${tokens[1]}" at line ${
+                        i + 1
+                      }, expected either "custom", "arx" or "off"`,
+                    )
+                  }
                 }
-              }
-              break
-            case 'wall':
-              {
-                const wall = roomDefinitions[currentBlock.name].textures.wall as QuadrupleOf<TextureDefinition>
+                break
+              case 'wall':
+                {
+                  const wall = roomDefinitions[currentBlock.name].textures.wall as QuadrupleOf<TextureDefinition>
 
-                if (tokens[1] === 'custom') {
-                  const texture = Texture.fromCustomFile({ filename: tokens[3], sourcePath: tokens[2] })
-                  const fitX = tokens[4] === 'fit-x'
-                  const fitY = tokens[4] === 'fit-y'
+                  if (tokens[1] === 'custom') {
+                    const texture = Texture.fromCustomFile({ filename: tokens[3], sourcePath: tokens[2] })
+                    const fitX = tokens[4] === 'fit-x'
+                    const fitY = tokens[4] === 'fit-y'
 
-                  wall[0] = { texture, fitX, fitY, isRemoved: false }
-                  wall[1] = { texture, fitX, fitY, isRemoved: false }
-                  wall[2] = { texture, fitX, fitY, isRemoved: false }
-                  wall[3] = { texture, fitX, fitY, isRemoved: false }
-                } else if (tokens[1] === 'arx') {
-                  // TODO: an arx texture might not always be 128x128
-                  const textureSize = 128
+                    wall[0] = { texture, fitX, fitY, isRemoved: false }
+                    wall[1] = { texture, fitX, fitY, isRemoved: false }
+                    wall[2] = { texture, fitX, fitY, isRemoved: false }
+                    wall[3] = { texture, fitX, fitY, isRemoved: false }
+                  } else if (tokens[1] === 'arx') {
+                    // TODO: an arx texture might not always be 128x128
+                    const textureSize = 128
 
-                  const texture = new Texture({ filename: tokens[2], size: textureSize })
-                  const fitX = tokens[3] === 'fit-x'
-                  const fitY = tokens[3] === 'fit-y'
+                    const texture = new Texture({ filename: tokens[2], size: textureSize })
+                    const fitX = tokens[3] === 'fit-x'
+                    const fitY = tokens[3] === 'fit-y'
 
-                  wall[0] = { texture, fitX, fitY, isRemoved: false }
-                  wall[1] = { texture, fitX, fitY, isRemoved: false }
-                  wall[2] = { texture, fitX, fitY, isRemoved: false }
-                  wall[3] = { texture, fitX, fitY, isRemoved: false }
-                } else if (tokens[1] === 'removed') {
-                  wall[0].isRemoved = true
-                  wall[1].isRemoved = true
-                  wall[2].isRemoved = true
-                  wall[3].isRemoved = true
-                } else {
-                  console.error(
-                    `[error] loadRooms: Unknown texture type "${tokens[1]}" at line ${
-                      i + 1
-                    }, expected either "custom", "arx", or "removed"`,
-                  )
+                    wall[0] = { texture, fitX, fitY, isRemoved: false }
+                    wall[1] = { texture, fitX, fitY, isRemoved: false }
+                    wall[2] = { texture, fitX, fitY, isRemoved: false }
+                    wall[3] = { texture, fitX, fitY, isRemoved: false }
+                  } else if (tokens[1] === 'removed') {
+                    wall[0].isRemoved = true
+                    wall[1].isRemoved = true
+                    wall[2].isRemoved = true
+                    wall[3].isRemoved = true
+                  } else {
+                    console.error(
+                      `[error] loadRooms: Unknown texture type "${tokens[1]}" at line ${
+                        i + 1
+                      }, expected either "custom", "arx", or "removed"`,
+                    )
+                  }
                 }
-              }
-              break
-            case 'wall-north':
-            case 'wall-east':
-            case 'wall-south':
-            case 'wall-west':
-              {
-                const wallIdx = tokens[0].endsWith('north')
-                  ? 0
-                  : tokens[0].endsWith('east')
-                  ? 1
-                  : tokens[0].endsWith('south')
-                  ? 2
-                  : 3
+                break
+              case 'wall-north':
+              case 'wall-east':
+              case 'wall-south':
+              case 'wall-west':
+                {
+                  const wallIdx = tokens[0].endsWith('north')
+                    ? 0
+                    : tokens[0].endsWith('east')
+                    ? 1
+                    : tokens[0].endsWith('south')
+                    ? 2
+                    : 3
 
-                const wall = roomDefinitions[currentBlock.name].textures.wall as QuadrupleOf<TextureDefinition>
-                if (tokens[1] === 'custom') {
-                  const texture = Texture.fromCustomFile({ filename: tokens[3], sourcePath: tokens[2] })
-                  const fitX = tokens[4] === 'fit-x'
-                  const fitY = tokens[4] === 'fit-y'
+                  const wall = roomDefinitions[currentBlock.name].textures.wall as QuadrupleOf<TextureDefinition>
+                  if (tokens[1] === 'custom') {
+                    const texture = Texture.fromCustomFile({ filename: tokens[3], sourcePath: tokens[2] })
+                    const fitX = tokens[4] === 'fit-x'
+                    const fitY = tokens[4] === 'fit-y'
 
-                  wall[wallIdx] = { texture, fitX, fitY, isRemoved: false }
-                } else if (tokens[1] === 'arx') {
-                  // TODO: an arx texture might not always be 128x128
-                  const textureSize = 128
+                    wall[wallIdx] = { texture, fitX, fitY, isRemoved: false }
+                  } else if (tokens[1] === 'arx') {
+                    // TODO: an arx texture might not always be 128x128
+                    const textureSize = 128
 
-                  const texture = new Texture({ filename: tokens[2], size: textureSize })
-                  const fitX = tokens[3] === 'fit-x'
-                  const fitY = tokens[3] === 'fit-y'
+                    const texture = new Texture({ filename: tokens[2], size: textureSize })
+                    const fitX = tokens[3] === 'fit-x'
+                    const fitY = tokens[3] === 'fit-y'
 
-                  wall[wallIdx] = { texture, fitX, fitY, isRemoved: false }
-                } else if (tokens[1] === 'removed') {
-                  wall[wallIdx].isRemoved = true
-                } else {
-                  console.error(
-                    `[error] loadRooms: Unknown texture type "${tokens[1]}" at line ${
-                      i + 1
-                    }, expected either "custom", "arx" or "removed"`,
-                  )
+                    wall[wallIdx] = { texture, fitX, fitY, isRemoved: false }
+                  } else if (tokens[1] === 'removed') {
+                    wall[wallIdx].isRemoved = true
+                  } else {
+                    console.error(
+                      `[error] loadRooms: Unknown texture type "${tokens[1]}" at line ${
+                        i + 1
+                      }, expected either "custom", "arx" or "removed"`,
+                    )
+                  }
                 }
-              }
-              break
-            default:
-              console.error(
-                `[error] loadRooms: Unknown side "${tokens[0]}" at line ${
-                  i + 1
-                }, expected "floor", "ceiling", "wall", "wall-east", "wall-west", "wall-north" or "wall-south"`,
-              )
+                break
+              default:
+                console.error(
+                  `[error] loadRooms: Unknown side "${tokens[0]}" at line ${
+                    i + 1
+                  }, expected "floor", "ceiling", "wall", "wall-east", "wall-west", "wall-north" or "wall-south"`,
+                )
+            }
           }
+          break
+        case 'premium':
+          {
+            if (tokens[0] === '}') {
+              currentBlock = undefined
+              if (premiumLinesSkipped > 0) {
+                console.info(`[info] loadRooms: Skipped parsing ${premiumLinesSkipped} premium only lines`)
+              }
+            } else {
+              if (settings.variant === 'premium') {
+                switch (tokens[0]) {
+                  case 'room':
+                    parseRoomKeyword()
+                    break
+                  case 'with':
+                    parseWithKeyword()
+                    break
+                  case 'cursor':
+                    parseCursorKeyword()
+                    break
+                  default:
+                    if (tokens[0].startsWith('$')) {
+                      parseVariable()
+                    } else {
+                      console.error(`[error] loadRooms: Unknown command "${tokens[0]}" at line ${i + 1}`)
+                    }
+                }
+              } else {
+                premiumLinesSkipped++
+              }
+            }
+          }
+          break
       }
     } else {
       switch (tokens[0]) {
         case 'define':
-          if (typeof tokens[1] === undefined) {
+          if (typeof tokens[1] === 'undefined') {
             console.error(`[error] loadRooms: missing define block's name at line ${i + 1}`)
           } else {
             if (tokens[2] !== '{') {
               console.error(`[error] loadRooms: missing { at line ${i + 1}`)
             } else {
-              currentBlock = {
-                type: 'define',
-                name: tokens[1],
-              }
+              currentBlock = { type: 'define', name: tokens[1] }
+
               if (typeof roomDefinitions[tokens[1]] === 'undefined') {
                 roomDefinitions[tokens[1]] = {
                   textures: {
@@ -192,69 +298,32 @@ export const loadRooms = async (filename: string, settings: Settings) => {
             }
           }
           break
-        case 'room':
-          switch (tokens[1]) {
-            case 'add':
-              // TODO: validate arguments
-              const posX = parseInt(tokens[2].startsWith('$') ? variables[tokens[2]] : tokens[2])
-              const posY = parseInt(tokens[3].startsWith('$') ? variables[tokens[3]] : tokens[3])
-              const posZ = parseInt(tokens[4].startsWith('$') ? variables[tokens[4]] : tokens[4])
-              let definitionName = tokens[5]
-              if (typeof roomDefinitions[definitionName] === 'undefined') {
-                console.error(`[error] loadRooms: Unknown texture definition "${tokens[5]}" at line ${i + 1}`)
-                definitionName = 'default'
+        case 'premium':
+          {
+            if (tokens[1] !== 'only') {
+              console.error(`[error] loadRooms: expected 'only' keyword after 'premium' at line ${i + 1}`)
+            } else {
+              if (tokens[2] !== '{') {
+                console.error(`[error] loadRooms: missing { at line ${i + 1}`)
+              } else {
+                currentBlock = { type: 'premium' }
+                premiumLinesSkipped = 0
               }
-              const adjustments = tokens.slice(6) as CursorDir[]
-
-              rooms.addRoom(new Vector3(posX, posY, posZ), roomDefinitions[definitionName], ...adjustments)
-              break
-            default:
-              console.error(`[error] loadRooms: Unknown parameter "${tokens[1]}" after "room" at line ${i + 1}`)
+            }
           }
+          break
+        case 'room':
+          parseRoomKeyword()
           break
         case 'with':
-          switch (tokens[1]) {
-            case 'light':
-              if (rooms.currentRoom === undefined) {
-                // TODO: error: only add light if there's at least 1 room
-              } else {
-                const mainLight = createLight({
-                  radius: Math.max(cursor.newSize.x, cursor.newSize.y, cursor.newSize.z),
-                  position: new Vector3(cursor.cursor.x, cursor.cursor.y - cursor.newSize.y / 2, cursor.cursor.z),
-                })
-
-                rooms.currentRoom.lights.push(mainLight)
-              }
-              break
-            default:
-              console.error(`[error] loadRooms: Unknown parameter "${tokens[1]}" after "with" at line ${i + 1}`)
-          }
+          parseWithKeyword()
           break
         case 'cursor':
-          switch (tokens[1]) {
-            case 'save':
-              // TODO: check if tokens[2] exists
-              cursor.saveAs(tokens[2])
-              break
-            case 'restore':
-              // TODO: check if tokens[2] exists
-              cursor.restore(tokens[2])
-              break
-            default:
-              console.error(`[error] loadRooms: Unknown parameter "${tokens[1]}" after "cursor" at line ${i + 1}`)
-          }
+          parseCursorKeyword()
           break
         default:
           if (tokens[0].startsWith('$')) {
-            if (tokens[1] === '=') {
-              if (typeof tokens[2] !== 'undefined') {
-                variables[tokens[0]] = tokens[2]
-              } else {
-                console.error(`[error] loadRooms: Missing value for variable at line ${i + 1}`)
-              }
-            } else {
-              console.error(`[error] loadRooms: Unexpected token "${tokens[1]}" after variable at line ${i + 1}`)
-            }
+            parseVariable()
           } else {
             console.error(`[error] loadRooms: Unknown command "${tokens[0]}" at line ${i + 1}`)
           }
