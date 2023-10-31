eba29107fafc7284a380e85f13c95f610be0f7ed
Lajos Meszaros <m_lajos@hotmail.com>
2023-10-31 14:06:44 +0100

    refactor(tools/map): compare textures with Texture.equals

--- src/tools/map/removePolygonsByTexture.ts
+++ src/tools/map/removePolygonsByTexture.ts
@@ -1,10 +1,7 @@
-import path from 'node:path'
 import { ArxMap } from '@src/ArxMap.js'
 import { Texture } from '@src/Texture.js'
 
 export const removePolygonsByTexture = (texture: Texture, map: ArxMap) => {
-  const { name: textureFilename } = path.parse(texture.filename.toLowerCase())
-
   const numberOfPolygons = map.polygons.length
   for (let i = numberOfPolygons - 1; i > 0; i--) {
     const polygon = map.polygons[i]
@@ -13,8 +10,7 @@ export const removePolygonsByTexture = (texture: Texture, map: ArxMap) => {
       continue
     }
 
-    const { name: polygonTextureFilename } = path.parse(polygon.texture.filename.toLowerCase())
-    if (polygonTextureFilename === textureFilename) {
+    if (polygon.texture.equals(texture)) {
       map.polygons.splice(i, 1)
     }
   }
--- src/tools/map/removePolygonsByTextureWithinBox.ts
+++ src/tools/map/removePolygonsByTextureWithinBox.ts
@@ -1,11 +1,8 @@
-import path from 'node:path'
 import { Box3 } from 'three'
 import { ArxMap } from '@src/ArxMap.js'
 import { Texture } from '@src/Texture.js'
 
 export const removePolygonsByTextureWithinBox = (texture: Texture, box: Box3, map: ArxMap) => {
-  const { name: textureFilename } = path.parse(texture.filename.toLowerCase())
-
   const numberOfPolygons = map.polygons.length
   for (let i = numberOfPolygons - 1; i > 0; i--) {
     const polygon = map.polygons[i]
@@ -18,8 +15,7 @@ export const removePolygonsByTextureWithinBox = (texture: Texture, box: Box3, ma
       continue
     }
 
-    const { name: polygonTextureFilename } = path.parse(polygon.texture.filename.toLowerCase())
-    if (polygonTextureFilename === textureFilename) {
+    if (polygon.texture.equals(texture)) {
       map.polygons.splice(i, 1)
     }
   }
