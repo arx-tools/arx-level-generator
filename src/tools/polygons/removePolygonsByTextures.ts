b0341d2aa5a4593a4889e452aa25f614241e7f7f
Lajos Meszaros <m_lajos@hotmail.com>
2023-10-31 17:08:58 +0100

    refactor(tools/polygons): bulk remove polygons by filtering the indices first then grouping them up with groupSequences

--- src/tools/polygons/removePolygonsByTextures.ts
+++ src/tools/polygons/removePolygonsByTextures.ts
@@ -1,17 +1,23 @@
 import { Polygons } from '@src/Polygons.js'
 import { Texture } from '@src/Texture.js'
+import { groupSequences } from '@src/faux-ramda.js'
 
 export const removePolygonsByTextures = (textures: Texture[], polygons: Polygons) => {
   if (textures.length === 0) {
     return
   }
 
+  const toBeRemoved: number[] = []
+
   const numberOfPolygons = polygons.length
   for (let i = numberOfPolygons - 1; i > 0; i--) {
     const polygon = polygons[i]
-
     if (polygon.texture?.equalsAny(textures)) {
-      polygons.splice(i, 1)
+      toBeRemoved.push(i)
     }
   }
+
+  groupSequences(toBeRemoved).forEach(([start, size]) => {
+    polygons.splice(start, size)
+  })
 }
--- src/tools/polygons/removePolygonsWithinBox.ts
+++ src/tools/polygons/removePolygonsWithinBox.ts
@@ -1,13 +1,19 @@
 import { Box3 } from 'three'
 import { Polygons } from '@src/Polygons.js'
+import { groupSequences } from '@src/faux-ramda.js'
 
 export const removePolygonsWithinBox = (box: Box3, polygons: Polygons) => {
+  const toBeRemoved: number[] = []
+
   const numberOfPolygons = polygons.length
   for (let i = numberOfPolygons - 1; i > 0; i--) {
     const polygon = polygons[i]
-
     if (polygon.isWithin(box)) {
-      polygons.splice(i, 1)
+      toBeRemoved.push(i)
     }
   }
+
+  groupSequences(toBeRemoved).forEach(([start, size]) => {
+    polygons.splice(start, size)
+  })
 }
