33a04b12912f40b41582a1058dfe4ca36aa575fd
Lajos Meszaros <m_lajos@hotmail.com>
2023-10-31 14:20:03 +0100

    refactor(tools): move tools/map to tools/polygons

--- src/tools/map/index.ts
+++
@@ -1,3 +0,0 @@
-export { makePolygonsDoubleSided } from '@tools/map/makePolygonsDoubleSided.js'
-export { removePolygonsByTextures } from '@tools/map/removePolygonsByTextures.js'
-export { removePolygonsWithinBox } from '@tools/map/removePolygonsWithinBox.js'
---
+++ src/tools/polygons/index.ts
@@ -0,0 +1,3 @@
+export { makePolygonsDoubleSided } from '@tools/polygons/makePolygonsDoubleSided.js'
+export { removePolygonsByTextures } from '@tools/polygons/removePolygonsByTextures.js'
+export { removePolygonsWithinBox } from '@tools/polygons/removePolygonsWithinBox.js'
