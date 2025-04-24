fe37e8e27163904a98b8780726f5b39e7d48c5a2
Lajos Meszaros <m_lajos@hotmail.com>
2025-04-24 14:18:42 +0200

    refactor(Settings): clean up constructor by using methods + change modes values to be like lightingCalculatorModes

--- src/Settings.ts
+++ src/Settings.ts
@@ -7,19 +7,31 @@ import { randomIntBetween } from '@src/random.js'
 
 dotenvConfig()
 
+// -----------
+
 const lightingCalculatorModes = ['MaxBrightness', 'CompleteDarkness', 'Arx', 'Realistic'] as const
 
 type LightingCalculatorMode = (typeof lightingCalculatorModes)[number]
+
+const defaultLightingCalculatorMode = 'Arx'
 
 function isValidLightingCalculatorMode(input: any): input is LightingCalculatorMode {
   return typeof input === 'string' && (lightingCalculatorModes as readonly string[]).includes(input)
 }
 
+// -----------
+
+const modes = ['development', 'production'] as const
+
+type Mode = (typeof modes)[number]
+
+const defaultMode: Mode = 'production'
+
 function isValidMode(input: any): input is 'development' | 'production' {
-  return input === 'development' || input === 'production'
+  return typeof input === 'string' && (modes as readonly string[]).includes(input)
 }
 
-type Modes = 'development' | 'production'
+// -----------
 
 export type SettingsConstructorProps = {
   /**
@@ -104,7 +116,7 @@ export type SettingsConstructorProps = {
    *
    * default value is "production"
    */
-  mode?: Modes
+  mode?: Mode
 
   /**
    * When this is set to true FTS files will not get compressed with pkware
@@ -190,7 +202,7 @@ export class Settings {
    *
    * default value is "production"
    */
-  readonly mode: Modes
+  readonly mode: Mode
 
   /**
    * arx-level-generator comes with its own assets folder
@@ -210,32 +222,15 @@ export class Settings {
 
     this.calculateLighting = props.calculateLighting ?? process.env.calculateLighting !== 'false'
 
-    let fallbackLCM: LightingCalculatorMode
-    if (isValidLightingCalculatorMode(process.env.lightingCalculatorMode)) {
-      fallbackLCM = process.env.lightingCalculatorMode
-    } else {
-      fallbackLCM = 'Arx'
-    }
+    this.lightingCalculatorMode = this.parseLightingCalculatorMode(props.lightingCalculatorMode)
 
-    this.lightingCalculatorMode = props.lightingCalculatorMode ?? fallbackLCM
-
-    let fallbackMode: Modes
-    if (isValidMode(process.env.mode)) {
-      fallbackMode = process.env.mode
-    } else {
-      fallbackMode = 'production'
-    }
-
-    this.mode = props.mode ?? fallbackMode
+    this.mode = this.parseMode(props.mode)
 
     this.uncompressedFTS = props.uncompressedFTS ?? process.env.uncompressedFTS === 'true'
 
     this.seed = props.seed ?? process.env.seed ?? this.createRandomSeed()
-    seedrandom(this.seed, { global: true })
 
-    const pathToThisFile = fileURLToPath(import.meta.url)
-    const dirContainingThisFile = path.dirname(pathToThisFile)
-    this.internalAssetsDir = path.resolve(dirContainingThisFile, '../assets')
+    this.internalAssetsDir = this.getInternalAssetsDir()
 
     this.assetsDir = props.assetsDir ?? process.env.assetsDir ?? path.resolve('./assets')
 
@@ -243,10 +238,45 @@ export class Settings {
       props.originalLevelFiles ?? process.env.originalLevelFiles ?? path.resolve('../pkware-test-files')
 
     this.cacheDir = props.cacheDir ?? process.env.cacheDir ?? path.resolve('./cache')
+
     this.outputDir = props.outputDir ?? process.env.outputDir ?? path.resolve('./output')
+
+    seedrandom(this.seed, { global: true })
   }
 
   private createRandomSeed(): string {
     return randomIntBetween(100_000_000, 999_999_999).toString()
+  }
+
+  private parseLightingCalculatorMode(
+    lightingCalculatorMode: SettingsConstructorProps['lightingCalculatorMode'],
+  ): LightingCalculatorMode {
+    if (lightingCalculatorMode !== undefined) {
+      return lightingCalculatorMode
+    }
+
+    if (isValidLightingCalculatorMode(process.env.lightingCalculatorMode)) {
+      return process.env.lightingCalculatorMode
+    }
+
+    return defaultLightingCalculatorMode
+  }
+
+  private parseMode(mode: SettingsConstructorProps['mode']): Mode {
+    if (mode !== undefined) {
+      return mode
+    }
+
+    if (isValidMode(process.env.mode)) {
+      return process.env.mode
+    }
+
+    return defaultMode
+  }
+
+  private getInternalAssetsDir(): string {
+    const pathToThisFile = fileURLToPath(import.meta.url)
+    const dirContainingThisFile = path.dirname(pathToThisFile)
+    return path.resolve(dirContainingThisFile, '../assets')
   }
 }
